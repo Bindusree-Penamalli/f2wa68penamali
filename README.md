dd31d6cb4c9b6888cab47e20e8ce8902a0b13c19
Bindusree-Penamalli <112150519+Bindusree-Penamalli@users.noreply.github.com>
2022-10-26 14:44:28 -0500

    new endpoint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # f2wa68penamali
 README
+[Hosted at](https://bindusree.onrender.com)
---
+++ routes/mydata.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* Correct */
+router.get('/', function(req, res, next) {
+  res.render('mydata', { title: 'BindusreePenamalli' });
+});
+
+module.exports = router;
