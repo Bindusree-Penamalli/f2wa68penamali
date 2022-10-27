158b947dc6b574f9482fa7ed63597d0b00cf63fb
Bindusree-Penamalli <112150519+Bindusree-Penamalli@users.noreply.github.com>
2022-10-26 21:14:13 -0500

    New endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* Correct */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BindusreePenamalli' });
+  res.render('mydata', { title: 'BindusreePenamalli' });
 });
 
-module.exports = router;
